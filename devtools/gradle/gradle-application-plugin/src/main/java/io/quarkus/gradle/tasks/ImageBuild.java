aa8556377e6865cd239a661b146d3f3c9d938b5c
Ioannis Canellos <iocanel@gmail.com>
2022-11-01 20:16:11 +0200

    feat: warn gradle user about missing container ext

--- devtools/gradle/gradle-application-plugin/src/main/java/io/quarkus/gradle/tasks/ImageBuild.java
+++ devtools/gradle/gradle-application-plugin/src/main/java/io/quarkus/gradle/tasks/ImageBuild.java
@@ -1,11 +1,36 @@
 
 package io.quarkus.gradle.tasks;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
+import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.tasks.options.Option;
+
+import io.quarkus.gradle.GradleUtils;
+import io.quarkus.maven.dependency.ArtifactCoords;
+import io.quarkus.maven.dependency.ArtifactDependency;
+import io.quarkus.maven.dependency.Dependency;
+
 public abstract class ImageBuild extends ImageTask {
+
+    public enum Builder {
+        docker,
+        jib,
+        buildpack,
+        openshift
+    }
+
+    Builder builder = Builder.docker;
+
+    @Option(option = "builder", description = "The container image extension to use for building the image (e.g. docker, jib, buildpack, openshift).")
+    public void setBuilder(Builder builder) {
+        this.builder = builder;
+    }
 
     @Inject
     public ImageBuild(QuarkusBuildConfiguration buildConfiguration) {
@@ -13,7 +38,32 @@ public abstract class ImageBuild extends ImageTask {
     }
 
     @Override
+    public List<Dependency> forcedDependencies() {
+        String quarkusVersion = GradleUtils.getQuarkusCoreVersion(getProject());
+        return Arrays.asList(new ArtifactDependency("io.quarkus", "quarkus-container-image-" + builder.name(), null,
+                ArtifactCoords.TYPE_JAR, quarkusVersion));
+    }
+
+    @Override
     public Map<String, String> forcedProperties() {
-        return Map.of("quarkus.container-image.build", "true");
+        return Map.of("quarkus.container-image.build", "true",
+                "quarkus.container-image.builder", builder.name());
+    }
+
+    @TaskAction
+    public void checkRequiredExtensions() {
+        // Currently forcedDependencies() is not implemented for gradle.
+        // So, let's give users a meaningful warning message.
+        String requiredExtension = "quarkus-container-image-" + builder.name();
+        String requiredDependency = requiredExtension + "-deployment";
+        List<String> projectDependencies = getProject().getConfigurations().stream().flatMap(c -> c.getDependencies().stream())
+                .map(d -> d.getName())
+                .collect(Collectors.toList());
+
+        if (!projectDependencies.contains(requiredDependency)) {
+            getProject().getLogger().warn("Task: {} requires extensions: {}", getName(), requiredDependency);
+            getProject().getLogger().warn("To add the extensions to the project you can run the following command:");
+            getProject().getLogger().warn("\tgradle addExtension --extensions={}", requiredExtension);
+        }
     }
 }
--- devtools/gradle/gradle-application-plugin/src/main/java/io/quarkus/gradle/tasks/ImagePush.java
+++ devtools/gradle/gradle-application-plugin/src/main/java/io/quarkus/gradle/tasks/ImagePush.java
@@ -1,9 +1,19 @@
 
 package io.quarkus.gradle.tasks;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
+
+import org.gradle.api.tasks.TaskAction;
+
+import io.quarkus.gradle.GradleUtils;
+import io.quarkus.maven.dependency.ArtifactCoords;
+import io.quarkus.maven.dependency.ArtifactDependency;
+import io.quarkus.maven.dependency.Dependency;
 
 public abstract class ImagePush extends ImageTask {
 
@@ -13,7 +23,37 @@ public abstract class ImagePush extends ImageTask {
     }
 
     @Override
+    public List<Dependency> forcedDependencies() {
+        String quarkusVersion = GradleUtils.getQuarkusCoreVersion(getProject());
+        return Arrays.asList(
+                new ArtifactDependency("io.quarkus", "quarkus-container-image", null, ArtifactCoords.TYPE_JAR, quarkusVersion));
+    }
+
+    @Override
     public Map<String, String> forcedProperties() {
         return Map.of("quarkus.container-image.push", "true");
+    }
+
+    @TaskAction
+    public void checkRequiredExtensions() {
+        List<String> containerImageExtensions = getProject().getConfigurations().stream()
+                .flatMap(c -> c.getDependencies().stream())
+                .map(d -> d.getName())
+                .filter(n -> n.startsWith("quarkus-container-image"))
+                .map(n -> n.replaceAll("-deployment$", ""))
+                .collect(Collectors.toList());
+
+        List<String> extensions = Arrays.stream(ImageBuild.Builder.values()).map(b -> "quarkus-container-image-" + b.name())
+                .collect(Collectors.toList());
+
+        if (containerImageExtensions.isEmpty()) {
+            getProject().getLogger().warn("Task: {} requires a container image extension.", getName());
+            getProject().getLogger().warn("Available container image exntesions: [{}]",
+                    extensions.stream().collect(Collectors.joining(", ")));
+            getProject().getLogger().warn("To add an extension to the project, you can run one of the commands below:");
+            extensions.forEach(e -> {
+                getProject().getLogger().warn("\tgradle addExtension --extensions={}", e);
+            });
+        }
     }
 }
